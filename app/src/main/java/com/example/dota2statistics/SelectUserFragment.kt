9774efeae26fca51574dedd93241353b0bba29dd
Tony <jantoniosanchez1991@gmail.com>
2022-07-13 09:59:05 +0200

    New changes

--- app/src/main/java/com/example/dota2statistics/SelectUserFragment.kt
+++ app/src/main/java/com/example/dota2statistics/SelectUserFragment.kt
@@ -3,8 +3,10 @@ package com.example.dota2statistics
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dota2statistics.data.models.byID.Profile
@@ -12,6 +14,7 @@ import com.example.dota2statistics.databinding.FragmentSelectUserBinding
 import com.example.dota2statistics.presentation.viewmodels.EmptyHomeViewModel
 import com.example.dota2statistics.presentation.viewmodels.EmptyHomeViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.Duration
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -35,6 +38,10 @@ class SelectUserFragment : Fragment(R.layout.fragment_select_user) {
         val args: SelectUserFragmentArgs by navArgs()
         val userName = args.user
         recyclerAdapter = UsersRecyclerAdapter()
+        recyclerAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply { putSerializable("profile", it) }
+            findNavController().navigate(R.id.action_selectUserFragment_to_userTabbedInfo, bundle)
+        }
         makeRequest(userName)
     }
 
